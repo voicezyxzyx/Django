21abc0bea36b284f7f401b460da41d3604e48c7f
voicezyxzyx <450538542.com>
2018-08-25 10:48:51 +0800

    模板index

--- test1/booktest/admin.py
+++ test1/booktest/admin.py
@@ -3,15 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from models import *
+class HeroInfoInline(admin.TabularInline):
+    model=HeroInfo
+    extra=3
+
 class BookInfoAdmin(admin.ModelAdmin):
     list_display = ['id','btitle','bpub_date']
     list_filter = ['btitle']
     search_fields = ['btitle']
-    list_per_page = 1
+    list_per_page = 10
     fieldsets = [
         ('base',{'fields':['btitle']}),
         ('super',{'fields':['bpub_date']})
     ]
+    inlines=[HeroInfoInline]
+
 admin.site.register(BookInfo,BookInfoAdmin)
 admin.site.register(HeroInfo)
 
--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import  *
+from django.template import RequestContext,loader
+def index(request):
+    temp=loader.get_template('booktest/index.html')
 
+    return HttpResponse(temp.render())
 # Create your views here.
